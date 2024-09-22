ba8343c97699370d151ae3414fe74d7df8b5d78a
Damiano Lozzi <damianolozzi1989@gmail.com>
2024-09-22 20:58:04 +0200

    changed setup.py

--- setup.py
+++ setup.py
@@ -1,22 +1,21 @@
-from setuptools import setup,find_packages
+from setuptools import setup, find_packages
 
 setup(
     name="text_generation",
     version="0.1.0",
-    author="Damiano Lozzi",
-    author_email="damianolozzi1989@gmail.com",
-    description="A text generation module using languagemodels",
-    long_description=open("README.md").read(),
-    long_description_content_type="text/markdown",
     packages=find_packages(),
     install_requires=[
         "languagemodels==0.20.0",
     ],
+    description="A text generation module using languagemodels",
+    author='DamianoLozzi',
+    author_email='damianolozzi1989@gmail.com',
+    url='',
+    python_requires='>=3.6',
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-    python_requires='>=3.6',
     test_suite='tests',
 )
