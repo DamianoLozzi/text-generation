808463ea48812efc6237a5f1418ea8f952ea9a06
Damiano Lozzi <damianolozzi1989@gmail.com>
2024-09-22 20:53:05 +0200

    removed url

--- setup.py
+++ setup.py
@@ -8,7 +8,6 @@ setup(
     description="A text generation module using languagemodels",
     long_description=open("README.md").read(),
     long_description_content_type="text/markdown",
-    url="https://github.com/DamianoLozzi/text-generation",
     packages=find_packages(),
     install_requires=[
         "languagemodels==0.20.0",
